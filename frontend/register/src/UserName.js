cb48f9198a0ab076e5c30e967768408e9d84f651
Seung Woon <113246980+Usimth@users.noreply.github.com>
2023-09-13 19:01:43 +0900

    Feat : UserName Page 구현

--- frontend/register/src/UserName.js
+++ frontend/register/src/UserName.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./Common.css";
 import "./UserName.css";
 
 export default function UserName() {
     const navigate = useNavigate(); // useNavigate hook to get the navigate function
+    const [inputValue, setInputValue] = useState('');
+    //inputValue에 사용자의 입력값이 담기게 된다. {inputValue}로 확인가능
+    const handleChange = (event) => {
+      setInputValue(event.target.value);
+    }
 
     return (
       <div>
         <set>
           <header>Easy KIOSK</header>
           <body>
-            body 자리
+              <div className="upper-t">사용자명을 입력해주세요</div>
+              <br/>
+              <input className ="input-des" type="text" value={inputValue} onChange={handleChange} />
           </body>
           <footer>
           <div className="blinking-text">나의 정보를 등록하세요 2/5</div>
