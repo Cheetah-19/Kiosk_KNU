4057a1ef1e184abef92f1abab031b194877e2fc7
WannaBeTop <optimuslove0223@icloud.com>
2023-09-20 21:25:54 +0900

    testing..
    연동 잘되는지 테스팅...

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('log_in/', views.login, name='login'),
-
     path('keyosk/', views.keyosk, name = 'keyosk'),
     path('non_member/keyosk/', views.keyosk, name = 'keyosk_nonmem'),
 ]
