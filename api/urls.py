a96d3b0dcd2a36c574cea4bda5bb2cc5cd9f7f77
WannaBeTop <optimuslove0223@icloud.com>
2023-09-21 00:38:37 +0900

    Feat: DRF 라우터 생성하기
    복잡한 URL 설정 없이도 RESTful API 구조를 손쉽게 제작해주는 DRF 라우터를 활용하여 코딩..

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,24 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
+from .views import MenuViewSet 
 
+#DRF의 DefaultRouter를 활용해 MenuViewSet에 대한
+#URL 패턴을 자동으로 생성
+#라우터는 MenuVIewSet를 'menus'라는 기본 URL에 등록
+
+router = routers.DefaultRouter()
+router.register('menus', MenuViewSet)
 
 #django의 URL 라우팅설정
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('log_in/', views.login, name='login'),
-    path('keyosk/', views.keyosk, name = 'keyosk'),
-    path('non_member/keyosk/', views.keyosk, name = 'keyosk_nonmem'),
+    path('', include(router.urls)),
+    # path('', views.index, name='index'),
+    # path('log_in/', views.login, name='login'),
+    # path('keyosk/', views.keyosk, name = 'keyosk'),
+    # path('non_member/keyosk/', views.keyosk, name = 'keyosk_nonmem'),
     path('create_menu/', views.create_menu, name='create_menu'),
+
 ]
 
