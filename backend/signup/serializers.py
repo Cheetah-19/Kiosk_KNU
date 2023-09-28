a90a716f0a6657f2e47e53dfded14394574293cf
WannaBeTop <143021741+WannaBeTop@users.noreply.github.com>
2023-09-29 00:09:42 +0900

    style: delete unnecessary code from views.py and serializers.py

--- backend/signup/serializers.py
+++ backend/signup/serializers.py
@@ -2,25 +2,6 @@ from rest_framework import serializers
 from .models import *
 from rest_framework.response import Response
 
-# class MenuSerializer(serializers.ModelSerializer):
-#     menu_ingredient = serializers.SlugRelatedField(
-#         many=True,
-#         queryset=Ingredient.objects.all(),
-#         slug_field='ingredient_name'
-#     )
-
-#     menu_option = serializers.SlugRelatedField(
-#         many=True,
-#         queryset=Option.objects.all(),
-#         slug_field='option_name'
-#     )
-
-
-#     class Meta:
-#         model = Menu
-#         fields = ['menu_name', 'menu_price', 'menu_introduction', 'menu_ingredient', 'menu_option']
-
-
 class UserSerializer(serializers.ModelSerializer):
     
     user_vegetarian = serializers.SlugRelatedField(
