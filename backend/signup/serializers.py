ac27f1654bcc99480f121c0db1fab65c17824411
WannaBeTop <optimuslove0223@icloud.com>
2023-10-06 00:30:03 +0900

    Feat : signup logic modified _ What to do when the user clicks not-applicable

--- backend/signup/serializers.py
+++ backend/signup/serializers.py
@@ -54,7 +54,7 @@ class UserSerializer(serializers.ModelSerializer):
             elif is_user_name_already_here:
                 error_message = '이미 등록된 사용자 이름입니다.'
             else:
-                error_message = '이미 등록된 사용자 이름입니다.'
+                error_message = '이미 등록된 전화번호입니다.'
             raise serializers.ValidationError({'message': error_message})
         
         else:
@@ -64,25 +64,22 @@ class UserSerializer(serializers.ModelSerializer):
             )
 
             vegetarian_name = validated_data.get('user_vegetarian')
-            allergy_names = validated_data.get('user_allergy')
             religion_type = validated_data.get('religion')
-
-            vegetarian =Vegetarian.objects.get(vegetarian_name = vegetarian_name)
-            user.vegetarian_id = vegetarian.id
+            allergy_names = validated_data.get('user_allergy')
 
             if vegetarian_name is not None:
-                vegetarian =Vegetarian.objects.get(vegetarian_name=vegetarian_name)
+                vegetarian = Vegetarian.objects.get(vegetarian_name=vegetarian_name)
                 user.vegetarian_id = vegetarian.id
+
+            if religion_type is not None:
+                religion = Religion.objects.get(religion_type=religion_type)
+                user.religion = religion
 
             if allergy_names is not None:
                 for allergy_name in allergy_names:
                     allergy = Allergy.objects.get(allergy_name=allergy_name)
                     user.user_allergy.add(allergy)
-
-            if religion_type is not None:
-                religion = Religion.objects.get(religion_type=religion_type)
-                user.religion_id= religion.id
-
+                    
             user.save()
 
             return user
