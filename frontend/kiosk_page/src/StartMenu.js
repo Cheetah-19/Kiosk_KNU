f5eff3bc17f846a31ee16d55c7647a1ad7218cff
Seung Woon <113246980+Usimth@users.noreply.github.com>
2023-11-17 00:18:44 +0900

    Fix : StartPage 디자인

--- frontend/kiosk_page/src/StartMenu.js
+++ frontend/kiosk_page/src/StartMenu.js
@@ -1,21 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import "./Home.css";
-
-
+import "./StartMenu.css";
 
 function StartMenu() {
   const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleLogin = () => {
     navigate('/logincheck');
-};
+  };
 
+  const handleGuestOrder = () => {
+    navigate('/MainMenu');
+  };
 
-    return (
-        <div id="start_img" onClick={handleSubmit}></div>
-      );
+  return (
+    <div className='container'>
+      <div className='logo-container'></div>
+      <button className='upper-button' onClick={handleLogin}>로그인하기</button>
+      <button className='lower-button' onClick={handleGuestOrder}>비회원 주문하기</button>
+    </div>
+  );
 }
 
 export default StartMenu;
