63cb2e6c9af8a558fa4efb79c851d948a42a540d
Seung Woon <113246980+Usimth@users.noreply.github.com>
2023-11-18 08:53:08 +0900

    Fix: SignUp.js

--- frontend/kiosk_page/src/SignUp.js
+++ frontend/kiosk_page/src/SignUp.js
@@ -12,14 +12,13 @@ export default function Pay() {
     function herf_home() {
       navigate('/');
     }
-
     return (
       <div id = "pay_page">
         <div id="top_bar_home" onClick={herf_home}></div>
         <header>Easy KIOSK</header>
         <div className='rect3'>
           <div className='qr-txt'>QR코드를 스캔하여 등록해주세요</div>
-          <div className='in-button'>처음으로</div>
+          <div className='in-button' onClick={herf_home}>처음으로</div>
         </div>
       </div>
     );
