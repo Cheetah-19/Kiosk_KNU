abaa9bf9edf1b4dda66ca76d5662ee889aacc5e1
Lucerna00 <pjs3859811@naver.com>
2023-11-26 22:19:41 +0900

    Fix : Pay 디자인 수정

--- frontend/kiosk_page/src/Pay.js
+++ frontend/kiosk_page/src/Pay.js
@@ -33,21 +33,26 @@ export default function Pay() {
         <div className='rect1'>
           <div className='txt1'>주문목록을 확인해주세요!</div>
           <div className="rect2">
-            <p>{option === '테이크아웃' ? '테이크아웃' : '매장식사'}</p>
+            <p style={{margin: '10px'}}>{option === '테이크아웃' ? '테이크아웃' : '매장식사'}</p>
             {cart.map((item, index) => (
-              <div key={index}>
-                <h2>{item.menu.menu_name}</h2>
-                {Object.entries(item.options).map(([optionName, quantity]) => (
-                  <p key={optionName}>{optionName}: {quantity}개</p>
-                ))}
-                <p>가격: {item.total.toLocaleString()}원</p>
+              <div>
+                <div id='order-list' key={index}>
+                  <div id='menu-name'>{item.menu.menu_name}</div>
+                  {Object.entries(item.options).map(([optionName, quantity]) => (
+                    <div id='option-name' key={optionName}>{optionName}+{quantity}</div>
+                  ))}
+                  <div id='total-price'>{item.total.toLocaleString()}원</div>
+                </div>
+                <div id='separator'></div>
               </div>
             ))}
           </div>
           <div className='sum-txt'>합계</div>
           <div className='sum-price'>{totalPrice.toLocaleString()}원</div>
-          <div className='prev-button' onClick={goBack}>이전으로 </div>
-          <div className='pay-button' onClick={goToPayCheck}>결제</div>
+          <div id='pay-btn-container'>
+            <div className='light-gray-btn' onClick={goBack}>이전으로 </div>
+            <div className='light-gray-btn' onClick={goToPayCheck}>결제하기</div>
+          </div>
         </div>
       </div>
     );
