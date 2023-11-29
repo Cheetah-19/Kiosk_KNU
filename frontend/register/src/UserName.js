0339c2ed68f7fa425cfe62b93921978ecccfaade
Dasu <ekgns1106@naver.com>
2023-11-30 02:46:29 +0900

    Fix : UserName css 수정 완료

--- frontend/register/src/UserName.js
+++ frontend/register/src/UserName.js
@@ -1,53 +1,80 @@
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+
+
 import "./Common.css";
-import "./UserName.css";
 
-export default function UserName() {
-    const navigate = useNavigate(); // useNavigate hook to get the navigate function
-    const location = useLocation();
-    const photos = location.state.photos;
-    const [inputValue, setInputValue] = useState('');
 
-    const handleChange = (event) => {
-      if (/^\d+$/.test(event.target.value)) {
-        alert('숫자는 입력할 수 없습니다.'); 
-      } else if (event.target.value.split(' ').join('') !== event.target.value) {
-        alert('공백은 입력할 수 없습니다.');
-      } else {
-        setInputValue(event.target.value);
-      }
+export default function FaceReco() {
+  const navigate = useNavigate(); // useNavigate hook to get the navigate function
+  const location = useLocation();
+  const photos = location.state.photos;
+
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event) => {
+    if (/^\d+$/.test(event.target.value)) {
+      alert('숫자는 입력할 수 없습니다.');
+    } else if (event.target.value.split(' ').join('') !== event.target.value) {
+      alert('공백은 입력할 수 없습니다.');
+    } else {
+      setInputValue(event.target.value);
     }
+  }
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
+  const handleNext = () => {
+    if (inputValue.trim().length === 0) {
+      alert('숫자 또는 공백을 입력할 수 없습니다.');
+    } else {
       navigate('/PhoneNum', { state: { inputValue, photos } });
-    };
+    }
+  };
 
-    const handleNext = () => {
-      if (inputValue.trim().length === 0) {
-        alert('숫자 또는 공백을 입력할 수 없습니다.');
-      } else {
-        navigate('/PhoneNum', { state: { inputValue, photos } });
-      }
-    };
+  const resetPhotos = () => {
+    navigate('/', { state: { photos: [] } });
+  };
 
-    return (
+  return (
+    <div>
       <div>
         <header>Easy KIOSK</header>
-        <div>
-          <div className="upper-t">사용자명을 입력해주세요</div>
-          <br/>
-          <form onSubmit={handleSubmit}>
-            <input className ="input-des" type="text" value={inputValue} onChange={handleChange} />
-          </form>
-        </div>
-        <footer>
-          <div className="blinking-text">나의 정보를 등록하세요 2/6</div>
-          <button className="next-button" onClick={handleNext}>다음으로</button>
-        </footer>
-        {/* User Name Content... */}
       </div>
-    );
+      <div>
+        <div className="Top_text">
+          <div className="title"> 내 정보 등록하기 </div>
+        </div>
+        <div className="Middle_Menu">
+          <div id="inner-bg">
+            <div className="middle_count">
+              <div className="middle_count_text">2/4</div>
+            </div>
+            <div className="middle_title">
+              <div className="middle_title_text">이름 등록하기</div>
+            </div>
+            <div className="middle_camera">
+              <input className="input-des" type="text" value={inputValue} onChange={handleChange} placeholder="이름을 입력해주세요" />
+            </div>
+
+          </div>
+        </div>
+        <div className="Bottom_button">
+          <div className="left_section">
+            <div id="left_button">
+              <div className="button_text" onClick={resetPhotos}> 이전으로 </div>
+            </div>
+          </div>
+          <div className="right_section">
+            <div id="right_button">
+              <div className="button_text" onClick={() => {
+                console.log(photos); // photos를 출력
+                handleNext(); // 다음 페이지로 이동
+              }}> 다음으로 </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
