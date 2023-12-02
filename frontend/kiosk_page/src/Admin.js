7f434cd8b167ebc2250c24ca5dcb3434c404ac18
Seung Woon <113246980+Usimth@users.noreply.github.com>
2023-12-03 01:08:28 +0900

    Feat : Admin.js 카테고리 추가

--- frontend/kiosk_page/src/Admin.js
+++ frontend/kiosk_page/src/Admin.js
@@ -15,6 +15,8 @@ export default function Admin() {
     const [phoneNumber, setPhoneNumber] = useState('');
     const [menuName, setMenuName] = useState('');
     const [image, setImage] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [category, setCategory] = useState(null);
 
     // 모달 상태 변수 및 함수 추가
     const [showModal, setShowModal] = useState(false);
@@ -24,6 +26,11 @@ export default function Admin() {
         setImage(null);
         setMenuName('');
         setShowModal(false);
+    };
+
+    const handleCategoryClick = (category) => {
+        setCategory(category);
+        setSelectedCategory(category);
     };
 
     const handleImageUpload = (e) => {
@@ -55,9 +62,11 @@ export default function Admin() {
             const formData = new FormData();
             formData.append('menuName', menuName);
             formData.append('image', image);
+            formData.append('category', selectedCategory);
             //Debug용 console log
             console.log('메뉴 이름', menuName);
             console.log('이미지:',image);
+            console.log('선택된 카테고리:', selectedCategory);
             const response = await axios.post(`${BASE_URL}/서버URL`, formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
@@ -91,14 +100,80 @@ export default function Admin() {
                     </div>
                 </Modal.Header>
                 <Modal.Body>
+                    <div>
+                        <h4 className="selected_menu">카테고리 선택</h4>
+                    </div>
                     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                        <div>카테고리</div>
+                        <div style={{ 
+                            display: 'flex', 
+                            justifyContent: 'space-between', 
+                            marginBottom: '5px' // 첫 번째 줄에 마진 추가
+                        }}>
+                            <div className="categories_btn" 
+                                onClick={() => handleCategoryClick(1)}
+                                style={{ 
+                                    backgroundColor: selectedCategory === 1 ? '#FF7A00' : '#E8E8E8', 
+                                    marginRight: '5px'
+                                }}
+                            >
+                                <span className="categories_btn_text" 
+                                    style={{ color: selectedCategory === 1 ? '#FFF' : '#000' }}
+                                >
+                                    라면류
+                                </span>
+                            </div>
+                            <div className="categories_btn" 
+                                onClick={() => handleCategoryClick(2)}
+                                style={{ 
+                                    backgroundColor: selectedCategory === 2 ? '#FF7A00' : '#E8E8E8', 
+                                    marginLeft: '5px'
+                                }}
+                            >
+                                <span className="categories_btn_text" 
+                                    style={{ color: selectedCategory === 2 ? '#FFF' : '#000' }}
+                                >
+                                    국수류
+                                </span>
+                            </div>
+                        </div>
+                        <div style={{ 
+                            display: 'flex', 
+                            justifyContent: 'space-between', 
+                            marginTop: '5px' // 두 번째 줄에 마진 추가
+                        }}>
+                            <div className="categories_btn" 
+                                onClick={() => handleCategoryClick(3)}
+                                style={{ 
+                                    backgroundColor: selectedCategory === 3 ? '#FF7A00' : '#E8E8E8',
+                                    marginRight: '5px'
+                                }}
+                            >
+                                <span className="categories_btn_text" 
+                                    style={{ color: selectedCategory === 3 ? '#FFF' : '#000' }}
+                                >
+                                    사이드 메뉴
+                                </span>
+                            </div>
+                            <div className="categories_btn" 
+                                onClick={() => handleCategoryClick(4)}
+                                style={{ 
+                                    backgroundColor: selectedCategory === 4 ? '#FF7A00' : '#E8E8E8',
+                                    marginLeft: '5px'
+                                }}
+                            >
+                                <span className="categories_btn_text" 
+                                    style={{ color: selectedCategory === 4 ? '#FFF' : '#000' }}
+                                >
+                                    추천메뉴
+                                </span>
+                            </div>
+                        </div>
                         <input 
                             type="file" 
                             accept="image/*" 
                             onChange={handleImageUpload} 
                             id="fileInput"
-                            style={{ display: 'none' }} // 파일 입력 필드 숨기기
+                            style={{ display: 'none' }} 
                         />
                         <label htmlFor="fileInput" style={{ cursor: 'pointer' }}>이미지 업로드(터치)</label>
                         {image && <img src={image} alt="Selected" style={{ width: '230px', height: '160px', objectFit: 'cover' }} />}
