cef70a523d561c3c8ddbd883536819afc16df967
WannaBeTop <optimuslove0223@icloud.com>
2023-12-06 01:55:09 +0900

    Add : Add the ability to dynamically import data from a DB, and filter menus and exclude menus that have already been eaten

--- backend/recommendation/views.py
+++ backend/recommendation/views.py
@@ -5,6 +5,7 @@ from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import *
+from .serializers import *
 from recommendation.models import *
 from gensim.models import Word2Vec
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
@@ -16,25 +17,64 @@ from sklearn.feature_extraction.text import CountVectorizer
 class TF_IDF_View(APIView):
     def get(self, request, user_id):
         # 메뉴와 재료
-        menus = {
-            '불닭 국수': '밀 양파 고추 닭고기 밀가루 계란',
-            '싸이버거': '치즈 양파 닭고기 ',
-            '불고기버거': '양파 돼지고기 밀 치즈',
-            '소고기 샐러드': '토마토 양파 소고기 대두 계란',
-            '새우 초밥': '쌀 새우',
-            '연어 초밥': '연어 쌀',
-            '쌀국수': '쌀 양파 계란',
-            '불닭볶음면': '밀 계란 양파',
-            '칼국수': '밀 양파',
-            '파송송 라면': '밀 파',
-            '닭가슴살 샐러드': '닭가슴살 양상추 토마토'
-        }
+        # menus = {
+        #     '불닭 국수': '밀 양파 고추 닭고기 밀가루 계란',
+        #     '싸이버거': '치즈 양파 닭고기 ',
+        #     '불고기버거': '양파 돼지고기 밀 치즈',
+        #     '소고기 샐러드': '토마토 양파 소고기 대두 계란',
+        #     '새우 초밥': '쌀 새우',
+        #     '연어 초밥': '연어 쌀',
+        #     '쌀국수': '쌀 양파 계란',
+        #     '불닭볶음면': '밀 계란 양파',
+        #     '칼국수': '밀 양파',
+        #     '파송송 라면': '밀 파',
+        #     '닭가슴살 샐러드': '닭가슴살 양상추 토마토',
+        #     '양상추 샐러드': '토마토 양파 양상추'
+        # }
+
+        # # 주문 데이터: {주문번호: {'user': 사용자 ID, 'menus': [주문한 메뉴 리스트]}}
+        # orders = {
+        #     '10000080': {'user': '00000000003', 'menus': ['닭가슴살 샐러드']},
+        #     '10000082': {'user': '00000000003', 'menus': ['쌀국수', '불닭볶음면']}
+        # }
+
+        # 메뉴와 재료
+        menus_db = Menu.objects.all()
+        user_instance = User.objects.get(user_phonenum = user_id)             #유저 인스턴스 가져오기
+
+        try:
+            user_preprocessed_data = PreprocessedData.objects.get(user=user_instance)
+            exclude_ingredient_str = user_preprocessed_data.excluded_ingredients
+        except PreprocessedData.DoesNotExist:
+            exclude_ingredient_str = ""
+
+        #String 을 Set 으로 변경하는 과정
+        # 중괄호 제거 후 쉼표로 분할
+        exclude_ingredient_list = exclude_ingredient_str[1:-1].split(',')
+        #문자열 -> 정수 변환 후 set 제작
+        excluded_ingredients = set(int(item.strip()) for item in exclude_ingredient_list)
+
+
+        menus = {}
+        for menu in menus_db:
+            ingredients = [ingredient.id for ingredient in menu.menu_ingredient.all()]
+            # 제외 재료가 포함된 메뉴는 건너뛰기
+            if any(ingredient in excluded_ingredients for ingredient in ingredients):
+                continue
+            ingredients_str = " ".join([ingredient.ingredient_name for ingredient in menu.menu_ingredient.all()])
+            menus[menu.menu_name] = ingredients_str
+        
+        print(menus)
 
         # 주문 데이터: {주문번호: {'user': 사용자 ID, 'menus': [주문한 메뉴 리스트]}}
-        orders = {
-            '10000080': {'user': '00000000003', 'menus': ['파송송 라면', '불닭 국수', '닭가슴살 샐러드']},
-            '10000082': {'user': '00000000003', 'menus': ['쌀국수', '불닭볶음면', '소고기 샐러드']}
-        }
+        orders_db = Order.objects.filter(user=user_instance)
+        print(orders_db)
+        orders = {}
+        for order in orders_db:
+            ordered_items = Ordered_Item.objects.filter(order=order)
+            orders[order.order_num] = {'user': order.user.user_phonenum, 'menus': [item.menu.menu_name for item in ordered_items]}
+
+        print(orders)
 
         # TF-IDF 변환
         vectorizer = TfidfVectorizer()
@@ -45,8 +85,15 @@ class TF_IDF_View(APIView):
         cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
         # 사용자의 과거 주문에서 메뉴 추출
-        past_orders = [order['menus'] for order in orders.values() if order['user'] == user_id]
-        past_menus = [menu for order in past_orders for menu in order]
+        past_orders = []
+        for order in orders.values():
+            if order['user'] == user_id:
+                past_orders.append(order['menus'])
+
+        past_menus = []
+        for order in past_orders:
+            for menu in order:
+                past_menus.append(menu)
 
         # 과거에 주문한 메뉴와 유사한 메뉴 찾기
         similar_menus = np.zeros(len(menus))
@@ -54,11 +101,32 @@ class TF_IDF_View(APIView):
             index = list(menus.keys()).index(menu)
             similar_menus += cosine_sim[index]
 
-        # 유사도가 높은 순서대로 메뉴 정렬
-        sorted_menus = sorted(list(menus.keys()), key=lambda x: similar_menus[list(menus.keys()).index(x)], reverse=True)
+        # 유사도 계산을 위한 인덱스를 얻는 함수
+        def get_index(menu):
+            menu_keys = list(menus.keys())
+            index = menu_keys.index(menu)
+            return similar_menus[index]
+
+        # 메뉴 리스트를 생성
+        menu_list = list(menus.keys())
+
+        # 유사도가 높은 순서대로 메뉴를 정렬
+        sorted_menus = []
+        for menu in menu_list:
+            sorted_menus.append((menu, get_index(menu)))
+
+        sorted_menus.sort(key=lambda x: x[1], reverse=True)
+
+        # 메뉴 이름만 추출
+        sorted_menus = [menu[0] for menu in sorted_menus]
+        # # 유사도가 높은 순서대로 메뉴 정렬
+        # sorted_menus = sorted(list(menus.keys()), key=lambda x: similar_menus[list(menus.keys()).index(x)], reverse=True)
 
         # 과거에 주문한 메뉴는 추천 목록에서 제외
-        recommended_menus = [menu for menu in sorted_menus if menu not in past_menus]
+        recommended_menus = []
+        for menu in sorted_menus:
+            if menu not in past_menus:
+                recommended_menus.append(menu)
 
         return Response({'recommended_menus': recommended_menus})
     
