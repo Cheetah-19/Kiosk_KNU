e2793a6b85dc17105ac11ef0ee12a5b313345a47
Lucerna00 <pjs3859811@naver.com>
2023-12-08 16:17:53 +0900

    Feat : Add sending user name

--- backend/face_recognition/face_identification.py
+++ backend/face_recognition/face_identification.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-# 한 user의 face info와 프론트에서 찍은 사진들의 embedding을 코사인 유사도 방식으로 거리 계산
+# 한 user의 face info와 프론트에서 찍은 사진의 embedding을 코사인 유사도 방식으로 거리 계산
 def findCosineDistance(db_list, target):
     a = np.dot(db_list, target)
     b = np.linalg.norm(db_list, axis=1)
@@ -10,6 +10,6 @@ def findCosineDistance(db_list, target):
     return 1 - (a / (b * c))
 
 
-# 한 user의 face info와 프론트에서 찍은 사진들의 embedding의 최단 거리 반환
+# 한 user의 face info와 프론트에서 찍은 사진의 embedding의 최단 거리 반환
 def identification(db_embedding_list, target_embedding):
     return np.min(findCosineDistance(db_embedding_list, target_embedding))
