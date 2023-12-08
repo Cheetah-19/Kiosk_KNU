5b4dc858b987234b3ffd24f3174765daf0f30142
Lucerna00 <pjs3859811@naver.com>
2023-12-08 12:43:03 +0900

    Perf : Change face identification logic
    Change face recognizer model
    Change calculating distance logic
    Add homomorphic filter

--- backend/face_recognition/face_identification.py
+++ backend/face_recognition/face_identification.py
@@ -1,8 +1,15 @@
-from deepface.commons import distance as dst
+import numpy as np
 
 
-def identification(target_embedding_img, db_embedding_img):
-    if target_embedding_img is not None:
-        return dst.findCosineDistance(target_embedding_img, db_embedding_img)
-    else:
-        return None
+# 한 user의 face info와 프론트에서 찍은 사진들의 embedding을 코사인 유사도 방식으로 거리 계산
+def findCosineDistance(db_list, target):
+    a = np.dot(db_list, target)
+    b = np.linalg.norm(db_list, axis=1)
+    c = np.sqrt(np.sum(np.multiply(target, target)))
+
+    return 1 - (a / (b * c))
+
+
+# 한 user의 face info와 프론트에서 찍은 사진들의 embedding의 최단 거리 반환
+def identification(db_embedding_list, target_embedding):
+    return np.min(findCosineDistance(db_embedding_list, target_embedding))
