Fix : equalize fuctiions sign up, login by face reco

backend/face_recognition/face_methods.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 # 프론트에서 받은 base64 리스트를 embedding 리스트로 변환
-def base_to_embedding(face_bases: list) -> list:
+def base_to_vector(face_bases: list) -> list:
     embedding_list = []
 
     for base in face_bases:
@@ -12,5 +12,4 @@ def base_to_embedding(face_bases: list) -> list:
 
         if input_embedding is not None:
             embedding_list.append(input_embedding)
-
-    return np.array(embedding_list)
+    return embedding_list

backend/login/views.py
@@ -44,10 +44,12 @@ class FaceLoginView(APIView):
             except:
                 return Response('')
 
-            # 2. base64 -> image -> embedding
-            target_embedding_list = base_to_embedding(face_bases)
+            # 2. base64 -> image -> vector
+            target_embedding_list = base_to_vector(face_bases)
             print("Received face data from front")
 
+            # 3. vector-> embedding
+            embedding_array =  np.array(target_embedding_list)
             # 3. 모든 user의 정보 불러오기
             user_table = User.objects.all()
 
@@ -61,7 +63,7 @@ class FaceLoginView(APIView):
 
                     # 4. 2번에서의 벡터와 user의 face info 거리 계산
                     distance = 1e9
-                    for target in target_embedding_list:
+                    for target in embedding_array:
                         distance = min(distance, identification(user_face_list, target))
 
                     print(f"{user.user_name}: {distance}")