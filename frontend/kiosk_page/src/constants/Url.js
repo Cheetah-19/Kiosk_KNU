ffb026d475934357359e5f462d5edda9df044570
WannaBeTop <optimuslove0223@icloud.com>
2023-12-17 00:39:04 +0900

    Add : add url

--- frontend/kiosk_page/src/constants/Url.js
+++ frontend/kiosk_page/src/constants/Url.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://kioskknu2023.run.goorm.site';
-// export const BASE_URL = 'http://127.0.0.1:8000';
+// export const BASE_URL = 'https://kioskknu2023.run.goorm.site';
+export const BASE_URL = 'http://127.0.0.1:8000';
